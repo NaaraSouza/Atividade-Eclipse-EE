8040710371da29d30e666aab52b0770b346ee7d8
Naara Souza <102614662+NaaraSouza@users.noreply.github.com>
2022-04-29 16:51:23 -0300

    Add files via upload

---
+++ FormularioDeCadastro - Copia/src/main/java/br/com/senai/naarasouza/application/copy/Config.java
@@ -0,0 +1,13 @@
+package br.com.senai.naarasouza.application.copy;
+
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+import javax.faces.annotation.FacesConfig.Version;
+
+@FacesConfig(version=Version.JSF_2_3)
+@ApplicationScoped
+
+public class Config {
+
+}
