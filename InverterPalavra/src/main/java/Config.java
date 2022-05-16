9c8999e1a4825baf7decbdf8c81ed626987330c7
Naara Souza <102614662+NaaraSouza@users.noreply.github.com>
2022-05-16 10:01:09 -0300

    Update Config.java

--- InverterPalavra/src/main/java/Config.java
+++ InverterPalavra/src/main/java/Config.java
@@ -4,8 +4,8 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.faces.annotation.FacesConfig;
 import javax.faces.annotation.FacesConfig.Version;
 
-@FacesConfig(version=Version.JSF_2_3)
-@ApplicationScoped
+@FacesConfig(version=Version.JSF_2_3)// Facet fornece,comportamentos e recursos que são associados ao JSF.
+@ApplicationScoped //É usado para indicar que só haverá um objeto da classe anotada com @ApplicationScoped na sua aplicação.
 
 public class Config {
 
