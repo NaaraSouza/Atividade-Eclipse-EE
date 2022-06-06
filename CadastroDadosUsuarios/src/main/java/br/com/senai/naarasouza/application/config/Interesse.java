222123a23dba63cb9e2b28f5841bee622bb34443
Naara Souza <102614662+NaaraSouza@users.noreply.github.com>
2022-06-06 10:22:48 -0300

    Update Interesse.java

--- CadastroDadosUsuarios/src/main/java/br/com/senai/naarasouza/application/config/Interesse.java
+++ CadastroDadosUsuarios/src/main/java/br/com/senai/naarasouza/application/config/Interesse.java
@@ -1,6 +1,6 @@
 package br.com.senai.naarasouza.application.config;
 
-public class Interesse extends Interesses {
+public class Interesse extends Interesses { //Usa-se extends quando você deseja aplicar herança á sua classe
 	
 	private Integer id;
 	private String area;
@@ -9,7 +9,7 @@ public class Interesse extends Interesses {
 	
 	public Interesse(Integer id, String area) {
 		super();
-		this.id = id;
+		this.id = id; ////O this é usado para referenciar a um atributo externo fora do metodo. Por exemplo ser dentro de um método se estiver dois atributos com o mesmo nome oque estiver com a palavra "this" estará se referenciando a um atributo fora do metodo.
 		this.area = area;
 	}
 	public Integer getId() {
